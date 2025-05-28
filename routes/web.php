dea3098eb60f28aa0d392529da85008d52d3104d
Anastasia Zavizion <a.zavizion.a@gmail.com>
2025-05-28 13:45:09 +0200

    Added base pages

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SignupController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\CarController;
 
 Route::get('/', [HomeController::class, 'index'] );
 
@@ -11,3 +12,7 @@ Route::view('/about', 'about')->name('about');
 
 Route::get('/signup', [SignupController::class, 'create'])->name('signup');
 Route::get('/login', [LoginController::class, 'create'])->name('login');
+
+Route::get('/cars/search', [CarController::class, 'search'])->name('cars.search');
+
+Route::resource('cars',CarController::class);
